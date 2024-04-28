0cfca680a49ec517ffa3672f3202485d81b285b0
CalebKornfein <caleb.kornfein@gmail.com>
2024-04-28 15:58:15 -0400

    Add processed data files

--- analysis.py
+++ analysis.py
@@ -1,11 +1,10 @@
 import pandas as pd
 from constants import *
-import matplotlib.pyplot as plt
-import seaborn as sns
 from collections import Counter
 from scipy import stats
 from statistics import stdev
 from math import sqrt
+
 
 def create_confints(df, frequent, infrequent, path):
     def stat(col):
@@ -15,23 +14,26 @@ def create_confints(df, frequent, infrequent, path):
 
         total = df.shape[0]
         ordering = kOrderingDict[col]
-        
+
         rows = []
         for c in ordering:
             if full_counter[c] <= 1:
-                rows.append([col, # column name
-                            c,   # value
-                            full_counter[c], # value count
-                            full_counter[c] / total * 100, # value as a percentage
-                            frequent_counter[c], # frequent_count
-                            frequent_counter[c] * 100, # frequent percentage
-                            'NA', # frequent upper bound 95% CI
-                            'NA', # frequent lower bound 95% CI
-                            infrequent_counter[c], # infrequent count
-                            infrequent_counter[c] * 100, # infrequent percentage
-                            'NA', # infrequent percentage upper
-                            'NA', # infrequent percentage lower
-                ])
+                rows.append(
+                    [
+                        col,  # column name
+                        c,  # value
+                        full_counter[c],  # value count
+                        full_counter[c] / total * 100,  # value as a percentage
+                        frequent_counter[c],  # frequent_count
+                        frequent_counter[c] * 100,  # frequent percentage
+                        "NA",  # frequent upper bound 95% CI
+                        "NA",  # frequent lower bound 95% CI
+                        infrequent_counter[c],  # infrequent count
+                        infrequent_counter[c] * 100,  # infrequent percentage
+                        "NA",  # infrequent percentage upper
+                        "NA",  # infrequent percentage lower
+                    ]
+                )
                 continue
 
             frequent_percent = frequent_counter[c] / full_counter[c] * 100
@@ -41,20 +43,31 @@ def create_confints(df, frequent, infrequent, path):
             print(c, len(fake))
             std = stdev(fake)
 
-            length =  1.95996 * std / sqrt(full_counter[c])
-            rows.append([col, # column name
-                        c,   # value
-                        full_counter[c], # value count
-                        round(full_counter[c] / total * 100, 2), # value as a percentage
-                        frequent_counter[c], # frequent_count
-                        round(frequent_percent, 2), # frequent percentage
-                        min(round(frequent_percent + length, 2), 100), # frequent upper bound 95% CI
-                        max(round(frequent_percent - length, 2), 0), # frequent lower bound 95% CI
-                        infrequent_counter[c], # infrequent count
-                        round(infrequent_percent, 2),# infrequent percentage
-                        min(round(infrequent_percent + length, 2), 100), # infrequent percentage upper
-                        max(round(infrequent_percent - length, 2), 0), # infrequent percentage lower
-            ])
+            length = 1.95996 * std / sqrt(full_counter[c])
+            rows.append(
+                [
+                    col,  # column name
+                    c,  # value
+                    full_counter[c],  # value count
+                    round(full_counter[c] / total * 100, 2),  # value as a percentage
+                    frequent_counter[c],  # frequent_count
+                    round(frequent_percent, 2),  # frequent percentage
+                    min(
+                        round(frequent_percent + length, 2), 100
+                    ),  # frequent upper bound 95% CI
+                    max(
+                        round(frequent_percent - length, 2), 0
+                    ),  # frequent lower bound 95% CI
+                    infrequent_counter[c],  # infrequent count
+                    round(infrequent_percent, 2),  # infrequent percentage
+                    min(
+                        round(infrequent_percent + length, 2), 100
+                    ),  # infrequent percentage upper
+                    max(
+                        round(infrequent_percent - length, 2), 0
+                    ),  # infrequent percentage lower
+                ]
+            )
         return rows
 
     cumulative_rows = []
@@ -64,20 +77,33 @@ def create_confints(df, frequent, infrequent, path):
             cumulative_rows.append(row)
 
     final = pd.DataFrame(cumulative_rows)
-    cols = ["Column", "Value", "Count", "Percent", "Frequent Count", "Frequent Percentage", "Frequent Upper Bound", "Frequent Lower Bound", "Infrequent Count", "Infrequent Percentage", "Infrequent Upper Bound", "Infrequent Lower Bound"]
+    cols = [
+        "Column",
+        "Value",
+        "Count",
+        "Percent",
+        "Frequent Count",
+        "Frequent Percentage",
+        "Frequent Upper Bound",
+        "Frequent Lower Bound",
+        "Infrequent Count",
+        "Infrequent Percentage",
+        "Infrequent Upper Bound",
+        "Infrequent Lower Bound",
+    ]
     final.columns = cols
-    
+
     final.to_csv(path, index=False)
 
     return final
 
-def create_summary(df, path):
 
-    frequent = df[df['Masking Categories'] == 'Frequent']
+def create_summary(df, path):
+    frequent = df[df["Masking Categories"] == "Frequent"]
     n_frequent = frequent.shape[0]
     print("Frequent", frequent.shape)
 
-    infrequent = df[df['Masking Categories'] == 'Infrequent']
+    infrequent = df[df["Masking Categories"] == "Infrequent"]
     n_infrequent = infrequent.shape[0]
     print("Infrequent", infrequent.shape)
 
@@ -88,20 +114,23 @@ def create_summary(df, path):
 
         total = df.shape[0]
         ordering = kOrderingDict[col]
-        
+
         rows = []
         for c in ordering:
             frequent_percent = frequent_counter[c] / n_frequent * 100
             infrequent_percent = infrequent_counter[c] / n_infrequent * 100
-            rows.append([col, # column name
-                        c,   # value
-                        full_counter[c], # value count
-                        round(full_counter[c] / total * 100, 2), # value as a percentage
-                        frequent_counter[c], # frequent_count
-                        round(frequent_percent, 2), # frequent percentage
-                        infrequent_counter[c], # infrequent count
-                        round(infrequent_percent, 2),# infrequent percentage
-            ])
+            rows.append(
+                [
+                    col,  # column name
+                    c,  # value
+                    full_counter[c],  # value count
+                    round(full_counter[c] / total * 100, 2),  # value as a percentage
+                    frequent_counter[c],  # frequent_count
+                    round(frequent_percent, 2),  # frequent percentage
+                    infrequent_counter[c],  # infrequent count
+                    round(infrequent_percent, 2),  # infrequent percentage
+                ]
+            )
         return rows
 
     cumulative_rows = []
@@ -111,62 +140,73 @@ def create_summary(df, path):
             cumulative_rows.append(row)
 
     final = pd.DataFrame(cumulative_rows)
-    cols = ["Column", "Value", "Count", "Percent", "Frequent Count", "Frequent Percentage", "Infrequent Count", "Infrequent Percentage"]
+    cols = [
+        "Column",
+        "Value",
+        "Count",
+        "Percent",
+        "Frequent Count",
+        "Frequent Percentage",
+        "Infrequent Count",
+        "Infrequent Percentage",
+    ]
     final.columns = cols
-    
+
     final.to_csv(path, index=False)
 
     return final
 
 
-
 def prettify(df, path, include_confints=False, r=2):
-
     def format_na(f):
-        if f == 'NA':
+        if f == "NA":
             return f
         else:
             return round(f, r)
 
     def format(row):
-        value = str(" ".join(row['Value'].split("_")))
-        count = row['Count']
-        percent = round(row['Percent'], r)
-        frequent_count = round(row['Frequent Count'], r)
-        frequent_percentage =  format_na(row['Frequent Percentage'])
-        infrequent_count = round(row['Infrequent Count'], r)
-        infrequent_percentage =  format_na(row['Infrequent Percentage'])
-        
-        if (include_confints):
-            frequent_upper = format_na(row['Frequent Upper Bound'])
-            frequent_lower = format_na(row['Frequent Lower Bound'])
-            infrequent_upper = format_na(row['Infrequent Upper Bound'])
-            infrequent_lower = format_na(row['Infrequent Lower Bound'])
+        value = str(" ".join(row["Value"].split("_")))
+        count = row["Count"]
+        percent = round(row["Percent"], r)
+        frequent_count = round(row["Frequent Count"], r)
+        frequent_percentage = format_na(row["Frequent Percentage"])
+        infrequent_count = round(row["Infrequent Count"], r)
+        infrequent_percentage = format_na(row["Infrequent Percentage"])
 
-            return [value, # Value
-                f'{count} ({percent}%)',
-                f'{frequent_count} ({frequent_percentage}%, {frequent_lower}%-{frequent_upper}%)',
-                f'{infrequent_count} ({infrequent_percentage}%, {infrequent_lower}%-{infrequent_upper}%)']
+        if include_confints:
+            frequent_upper = format_na(row["Frequent Upper Bound"])
+            frequent_lower = format_na(row["Frequent Lower Bound"])
+            infrequent_upper = format_na(row["Infrequent Upper Bound"])
+            infrequent_lower = format_na(row["Infrequent Lower Bound"])
+
+            return [
+                value,  # Value
+                f"{count} ({percent}%)",
+                f"{frequent_count} ({frequent_percentage}%, {frequent_lower}%-{frequent_upper}%)",
+                f"{infrequent_count} ({infrequent_percentage}%, {infrequent_lower}%-{infrequent_upper}%)",
+            ]
         else:
-            return [value, # Value
-                f'{count} ({percent}%)',
-                f'{frequent_count} ({frequent_percentage}%)',
-                f'{infrequent_count} ({infrequent_percentage}%)']
+            return [
+                value,  # Value
+                f"{count} ({percent}%)",
+                f"{frequent_count} ({frequent_percentage}%)",
+                f"{infrequent_count} ({infrequent_percentage}%)",
+            ]
 
-    
     rows = []
-    current_column = ''
+    current_column = ""
     for i, row in df.iterrows():
-        if row['Column'] != current_column:
-            current_column = row['Column']
-            rows.append([row['Column']])
+        if row["Column"] != current_column:
+            current_column = row["Column"]
+            rows.append([row["Column"]])
         rows.append(format(row))
 
     data = pd.DataFrame(rows)
     data.columns = ["Category", "Total", "Frequent", "Infrequent"]
-    data.to_csv(path,index=False)
+    data.to_csv(path, index=False)
 
-def diff_in_prop(x1, n1, x2, n2, alpha_confidence = 0.05, bonferroni=1):
+
+def diff_in_prop(x1, n1, x2, n2, alpha_confidence=0.05, bonferroni=1):
     p1 = x1 / n1
     p2 = x2 / n2
 
@@ -193,31 +233,41 @@ def diff_in_prop(x1, n1, x2, n2, alpha_confidence = 0.05, bonferroni=1):
 def calculate_chi_squared_test_of_independence(df, categories):
     n_tests = len(categories)
 
-    cols = ['Category', 'P-val', 'Adj. P-val','P-val (No Missing)', 'Adj. P-val (No Missing)']
+    cols = [
+        "Category",
+        "P-val",
+        "Adj. P-val",
+        "P-val (No Missing)",
+        "Adj. P-val (No Missing)",
+    ]
     data = []
 
     for c in categories:
-        full = df[df['Column'] == c]
+        full = df[df["Column"] == c]
         # With all values
         full_res = stats.chi2_contingency(
-            full[['Frequent Count', 'Infrequent Count']].values, correction=False
+            full[["Frequent Count", "Infrequent Count"]].values, correction=False
         )
         # Without missing values
-        no_missing = full[full['Value'] != 'Missing']
+        no_missing = full[full["Value"] != "Missing"]
         no_missing_res = stats.chi2_contingency(
-            no_missing[['Frequent Count', 'Infrequent Count']].values, correction=False
+            no_missing[["Frequent Count", "Infrequent Count"]].values, correction=False
         )
 
-        data.append([
-            c,
-            full_res.pvalue,
-            full_res.pvalue * n_tests,
-            no_missing_res.pvalue,
-            no_missing_res.pvalue * n_tests])
+        data.append(
+            [
+                c,
+                full_res.pvalue,
+                full_res.pvalue * n_tests,
+                no_missing_res.pvalue,
+                no_missing_res.pvalue * n_tests,
+            ]
+        )
 
     chi = pd.DataFrame(data)
     chi.columns = cols
     return chi
+
 
 def prettify_chi(chi, path, r=3):
     def format(val):
@@ -230,23 +280,22 @@ def prettify_chi(chi, path, r=3):
 
     pretty = []
     for i, row in chi.iterrows():
-        pretty.append([
-            row['Category'],
-            format(row['P-val']),
-            format(row['Adj. P-val']),
-            format(row['P-val (No Missing)']),
-            format(row['Adj. P-val (No Missing)'])
-        ])
+        pretty.append(
+            [
+                row["Category"],
+                format(row["P-val"]),
+                format(row["Adj. P-val"]),
+                format(row["P-val (No Missing)"]),
+                format(row["Adj. P-val (No Missing)"]),
+            ]
+        )
     pretty_df = pd.DataFrame(pretty)
     pretty_df.columns = chi.columns
     pretty_df.to_csv(path, index=False)
 
 
-
-
-
 def main():
-    df = pd.read_csv('data/processed_full.csv')
+    df = pd.read_csv("data/processed_full.csv")
 
     # Write summary tables to
     kSummaryPath = "results_v2/summary.csv"
@@ -255,48 +304,31 @@ def main():
     kPrettySummaryPath = "results_v2/pretty_summary.csv"
     prettify(summary, kPrettySummaryPath, include_confints=False, r=2)
 
-
     # Conduct Chi-Square Tests of Independence
     of_interest = [
-        'Age', 'Gender', 'Race', 'Prior Condition', 'Political Affiliation',
-        'Has Primary Care Physician', 'Education', 'Housing Status',
-        'Insurance Status', 'Internet', 'News', 'Health Care Professionals',
-        'Family or Friends', 'Pamphlets', 'Community Organizations',
-        'Coworkers or Classmates', 'Government Health Department', 'Other',
-        'No Information',
+        "Age",
+        "Gender",
+        "Race",
+        "Prior Condition",
+        "Political Affiliation",
+        "Has Primary Care Physician",
+        "Education",
+        "Housing Status",
+        "Insurance Status",
+        "Internet",
+        "News",
+        "Health Care Professionals",
+        "Family or Friends",
+        "Pamphlets",
+        "Community Organizations",
+        "Coworkers or Classmates",
+        "Government Health Department",
+        "Other",
+        "No Information",
     ]
     chi = calculate_chi_squared_test_of_independence(summary, of_interest)
     kChiPath = "results_v2/chi.csv"
     prettify_chi(chi, kChiPath, r=3)
-    
-    
-
-
-
-
-
-    # final = create_confints(df, frequent, infrequent, path)
-    
-    # prettify(final, 1)
-
-    # # Test, x1, n1, x2, n2
-    # diff_in_prop_test = [
-    #     ('Female - Male', 318, 367, 327, 410),
-    #     ('Democrat - Republican', 221, 251, 35, 64),
-    #     ('Physician - No Physician', 494, 587, 148, 178),
-    #     ('Housed - Unhoused', 591, 714, 57, 68),
-    #     ('All Single Races - White',
-    #         178 + 34 + 145 + 6 + 10 + 5 + 25,
-    #         201 + 36 + 159 + 6 + 12 + 5 + 29,
-    #         226, 311),
-    #     ('Prior Condition - No Prior Condition', 338, 402, 284, 351),
-    #     ('High School - Graduate', 262, 303, 78, 102),
-    # ]
-
-    # for name, x1, n1, x2, n2 in diff_in_prop_test:
-    #     print("-"*30)
-    #     print("Difference in proportion test for:", name)
-    #     print(diff_in_prop(x1, n1, x2, n2, alpha_confidence=0.05, bonferroni=7))
 
 
 if __name__ == "__main__":
--- clean.py
+++ clean.py
@@ -1,29 +1,32 @@
 import pandas as pd
 from constants import *
-from collections import Counter
+
 
 def fetch_age_stats(df):
     ages = []
     for i, row in df.iterrows():
-        if row['Age'] == 'unk': continue
-        ages.append(int(row['Age']))
-    
-    print(pd.DataFrame({'Age': ages}).describe())
+        if row["Age"] == "unk":
+            continue
+        ages.append(int(row["Age"]))
+
+    print(pd.DataFrame({"Age": ages}).describe())
+
 
 def validate(df):
-    validate_option(df, kRaceOptions, 'race')
-    validate_options(df, kInsuranceOptions, 'insurance')
-    
+    validate_option(df, kRaceOptions, "race")
+    validate_options(df, kInsuranceOptions, "insurance")
+
 
 def validate_option(df, options, option_type):
     subset = df[[option for option in options]]
     print(f"-----------------------------\nCleaning {option_type}:")
-    unique = pd.unique(subset).values.ravel('K')
+    unique = pd.unique(subset).values.ravel("K")
     print(f"Unique values: {unique}")
     print(f"Missing values: {subset.isnull().sum()}")
-    print(f"")
+    print("")
 
-    print(f"-----------------------------\n")
+    print("-----------------------------\n")
+
 
 def add_masking_behaviors(df):
     behaviors = []
@@ -31,56 +34,63 @@ def add_masking_behaviors(df):
     for i, row in subset.iterrows():
         behavior = []
         for option in kMaskFrequencyOptions:
-            if row[option] == 'yes':
+            if row[option] == "yes":
                 behavior.append(option)
 
         if len(behavior) == 0:
-            print(i, behavior, 'NO RESPONSE')
-            behaviors.append('No Responses')
+            print(i, behavior, "NO RESPONSE")
+            behaviors.append("No Responses")
         elif len(behavior) == 1:
             behaviors.append(behavior[0])
         else:
-            print(i, behavior, 'MULTIPLE RESPONSES')
-            behaviors.append('Multiple Responses')
+            print(i, behavior, "MULTIPLE RESPONSES")
+            behaviors.append("Multiple Responses")
     return behaviors
+
 
 def add_binary_masking_behaviors(vals):
     behaviors = []
 
     for val in vals:
-        if val == 'Mask_Indoors_Always' or val == 'Mask_Indoors_Majority' or val == 'Mask_Indoors_Half':
-            behaviors.append('Frequent')
-        elif val == 'Mask_Indoors_Infrequent' or val == 'Mask_Indoors_Never':
-            behaviors.append('Infrequent')
+        if (
+            val == "Mask_Indoors_Always"
+            or val == "Mask_Indoors_Majority"
+            or val == "Mask_Indoors_Half"
+        ):
+            behaviors.append("Frequent")
+        elif val == "Mask_Indoors_Infrequent" or val == "Mask_Indoors_Never":
+            behaviors.append("Infrequent")
         else:
-            behaviors.append('Other')
+            behaviors.append("Other")
     return behaviors
+
 
 def add_age(df):
     ages = []
     for i, row in df.iterrows():
-        age = row['Age']
+        age = row["Age"]
 
-        if age == 'unk':
-            ages.append('unk')
+        if age == "unk":
+            ages.append("unk")
             continue
         age = int(age)
 
         if 18 <= age <= 24:
-            ages.append('18-24')
+            ages.append("18-24")
         elif 25 <= age <= 34:
-            ages.append('25-34')
+            ages.append("25-34")
         elif 35 <= age <= 44:
-            ages.append('35-44')
+            ages.append("35-44")
         elif 45 <= age <= 64:
-            ages.append('45-64')
+            ages.append("45-64")
         elif 65 <= age <= 84:
-            ages.append('65-84')
+            ages.append("65-84")
         elif 85 <= age <= 99:
-            ages.append('84-99')
+            ages.append("84-99")
         elif age >= 100:
-            ages.append('100+')
+            ages.append("100+")
     return ages
+
 
 def add_race(df):
     races = []
@@ -88,7 +98,7 @@ def add_race(df):
     for i, row in subset.iterrows():
         race = []
         for option in kRaceOptions:
-            if row[option] == 'yes':
+            if row[option] == "yes":
                 race.append(option)
 
         if len(race) == 0:
@@ -97,8 +107,9 @@ def add_race(df):
             races.append(race[0])
         else:
             print(race)
-            races.append('Multiracial')
+            races.append("Multiracial")
     return races
+
 
 def add_politics(df):
     politics = []
@@ -106,78 +117,101 @@ def add_politics(df):
     for i, row in subset.iterrows():
         politic = []
         for option in kPoliticalOptions:
-            if row[option] == 'yes':
+            if row[option] == "yes":
                 politic.append(option)
 
         if len(politic) == 0:
             print(row)
-            politics.append('Missing')
+            politics.append("Missing")
         elif len(politic) == 1:
             politics.append(politic[0])
         else:
             print(politic)
-            politics.append('Multiparty')
+            politics.append("Multiparty")
     return politics
 
+
 def add_condition(df):
-    return list(df[['Condition']].values.ravel())
+    return list(df[["Condition"]].values.ravel())
+
 
 def add_housing(df):
-    return list(df[['Housing']].values.ravel())
+    return list(df[["Housing"]].values.ravel())
+
 
 def add_education(df):
-    return list(df[['Education']].values.ravel())
+    return list(df[["Education"]].values.ravel())
+
 
 def add_insurance(df):
-    return list(df[['Insurance']].values.ravel())
+    return list(df[["Insurance"]].values.ravel())
+
 
 def add_gender(df):
-    return list(df[['Gender']].values.ravel())
+    return list(df[["Gender"]].values.ravel())
+
 
 def add_doctor(df):
-    return list(df[['Has_Doctor']].values.ravel())
+    return list(df[["Has_Doctor"]].values.ravel())
+
 
 def no_response(row, options):
     for option in options:
-        if row[option] == 'yes':
+        if row[option] == "yes":
             return False
     return True
+
 
 def multiple_responses(row, options):
     response_counter = 0
     for option in options:
-        if row[option] == 'yes':
+        if row[option] == "yes":
             response_counter += 1
     return response_counter >= 2
 
+
 def valid(df):
-    return df.apply(lambda row: not(no_response(row, kMaskFrequencyOptions) or multiple_responses(row, kMaskFrequencyOptions)), axis=1)
+    return df.apply(
+        lambda row: not (
+            no_response(row, kMaskFrequencyOptions)
+            or multiple_responses(row, kMaskFrequencyOptions)
+        ),
+        axis=1,
+    )
+
 
 def answered_masking(df):
-    return df.apply(lambda row: not(row['Mask_Indoors_Unsure'] == 'yes' or row['Mask_Indoors_Do_Not_Go_Out'] == 'yes'), axis=1) 
+    return df.apply(
+        lambda row: not (
+            row["Mask_Indoors_Unsure"] == "yes"
+            or row["Mask_Indoors_Do_Not_Go_Out"] == "yes"
+        ),
+        axis=1,
+    )
+
 
 def process(df, output_path, summary_path):
     # Add all variables for analysis to a cleaned dataframe
     d = {}
 
-    d['Masking'] = add_masking_behaviors(df)
-    d['Masking Categories'] = add_binary_masking_behaviors(d['Masking'])
-    d['Age'] = add_age(df)
-    d['Gender'] = add_gender(df)
-    d['Race'] = add_race(df)
-    d['Prior Condition'] = add_condition(df)
-    d['Political Affiliation'] = add_politics(df)
-    d['Has Primary Care Physician'] = add_doctor(df)
-    d['Education'] = add_education(df)
-    d['Housing Status'] = add_housing(df)
-    d['Insurance Status'] = add_insurance(df)
-    
+    d["Masking"] = add_masking_behaviors(df)
+    d["Masking Categories"] = add_binary_masking_behaviors(d["Masking"])
+    d["Age"] = add_age(df)
+    d["Gender"] = add_gender(df)
+    d["Race"] = add_race(df)
+    d["Prior Condition"] = add_condition(df)
+    d["Political Affiliation"] = add_politics(df)
+    d["Has Primary Care Physician"] = add_doctor(df)
+    d["Education"] = add_education(df)
+    d["Housing Status"] = add_housing(df)
+    d["Insurance Status"] = add_insurance(df)
+
     for source in kCovidInfoSourceOptions:
         d[kReplaceDict[source]] = list(df[[source]].values.ravel())
 
     for reason in kReasonOptions:
         d[kReplaceDict[reason]] = list(df[[reason]].values.ravel())
-    
+
     processed = pd.DataFrame(d)
 
     # Make all the nice swaps so that the values print nicely :)
@@ -208,15 +242,41 @@ def main():
     answered = answered_masking(df)
     df = df[answered]
 
-    process(df, output_path='data/processed_full.csv', summary_path='results/summary_full.csv')
+    process(
+        df,
+        output_path="data/processed_full.csv",
+        summary_path="results/summary_full.csv",
+    )
 
     # Also look at only infrequent mask users
-    infrequent = df[df.apply(lambda row: row['Mask_Indoors_Infrequent'] == 'yes' or row['Mask_Indoors_Never'] == 'yes', axis=1)]
-    process(infrequent, output_path='data/processed_infrequent.csv', summary_path='results/summary_infrequent.csv')
+    infrequent = df[
+        df.apply(
+            lambda row: row["Mask_Indoors_Infrequent"] == "yes"
+            or row["Mask_Indoors_Never"] == "yes",
+            axis=1,
+        )
+    ]
+    process(
+        infrequent,
+        output_path="data/processed_infrequent.csv",
+        summary_path="results/summary_infrequent.csv",
+    )
 
     # And frequent mask users
-    frequent = df[df.apply(lambda row: row['Mask_Indoors_Always'] == 'yes' or row['Mask_Indoors_Majority'] == 'yes' or row['Mask_Indoors_Half'] == 'yes', axis=1)]
-    process(frequent, output_path='data/processed_frequent.csv', summary_path='results/summary_frequent.csv')
+    frequent = df[
+        df.apply(
+            lambda row: row["Mask_Indoors_Always"] == "yes"
+            or row["Mask_Indoors_Majority"] == "yes"
+            or row["Mask_Indoors_Half"] == "yes",
+            axis=1,
+        )
+    ]
+    process(
+        frequent,
+        output_path="data/processed_frequent.csv",
+        summary_path="results/summary_frequent.csv",
+    )
+
 
 if __name__ == "__main__":
     main()
